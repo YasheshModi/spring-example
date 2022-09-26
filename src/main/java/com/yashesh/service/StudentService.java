ad3133bf27c26a0a37b72d93424983875c6b8724
Nirav <nrvmodi@gmail.com>
2022-09-26 16:11:01 +0530

    initialize commit

--- src/main/java/com/yashesh/service/StudentService.java
+++ src/main/java/com/yashesh/service/StudentService.java
@@ -7,4 +7,8 @@ public interface StudentService {
 
     List<Student> getAllStudents();
     Student saveStudent(Student student);
+    Student getStudentById(Long id);
+    Student updateStudent(Student student);
+    void deleteStudentById(Long id);
+
 }
